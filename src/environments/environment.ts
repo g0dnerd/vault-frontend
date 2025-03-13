2d8f543cf3c614ca520e2a115435d45c5db21b1e
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-13 13:21:55 +0100

    Correct env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,15 @@
-export const dev = {
-  production: false,
-  name: 'dev',
-  apiUrl: 'http://localhost:3000/api',
-  webSocketUrl: 'http://localhost:3000',
-  userUploadUrl: '/images/',
-};
-
 // export const dev = {
-//   production: true,
-//   name: 'prod',
-//   apiUrl: 'https://vault-api.up.railway.app/api',
-//   webSocketUrl: 'https://vault-api.up.railway.app',
+//   production: false,
+//   name: 'dev',
+//   apiUrl: 'http://localhost:3000/api',
+//   webSocketUrl: 'http://localhost:3000',
 //   userUploadUrl: '/images/',
 // };
+
+export const dev = {
+  production: true,
+  name: 'prod',
+  apiUrl: 'https://vault-api.up.railway.app/api',
+  webSocketUrl: 'https://vault-api.up.railway.app',
+  userUploadUrl: '/images/',
+};
