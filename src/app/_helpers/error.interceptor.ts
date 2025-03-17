787b080131e3b3a1401bddc895b00aa5ab7706de
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-17 10:05:34 +0100

    Generalize error interceptor to re-throw error status and message

--- src/app/_helpers/error.interceptor.ts
+++ src/app/_helpers/error.interceptor.ts
@@ -1,25 +1,19 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { throwError, catchError } from 'rxjs';
 
-import { AuthAppState } from '../_store';
-import { logout } from '../_store/actions/auth.actions';
-
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
-  const authStore$ = inject(Store<AuthAppState>);
-
   return next(req).pipe(
-    catchError((err) => {
-      if ([401, 403].includes(err.status)) {
-        authStore$.dispatch(logout());
-        router.navigateByUrl('/account/login');
+    catchError((err: HttpErrorResponse) => {
+      if (err.status === 0) {
+        console.error('A client-side error occurred:', err.error);
+        return throwError(() => new Error(`${err.status}`));
+      } else {
+        console.error(
+          `A server-side error occurred: ${err.status}, body was: `,
+          err.error,
+        );
+        return throwError(() => err);
       }
-
-      const error = err.error?.message || err.statusText;
-      return throwError(() => error);
-    })
+    }),
   );
 };
--- src/app/_helpers/index.ts
+++ src/app/_helpers/index.ts
@@ -1,4 +1,6 @@
 export * from './auth.guard';
+export * from './draft-setup';
 export * from './error.interceptor';
 export * from './jwt.interceptor';
 export * from './match-form.validator';
+export * from './roles.guard';
