3e8e9bcba0f4956136a3e6cf9156c77bef44afc1
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-17 10:07:41 +0100

    REmove obsolete alert component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 
-import { AlertComponent } from './alert/alert.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AuthAppState } from './_store';
 import { logout, refreshAuth } from './_store/actions/auth.actions';
 
 @Component({
   standalone: true,
-  imports: [RouterOutlet, AlertComponent, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -18,7 +17,7 @@ import { logout, refreshAuth } from './_store/actions/auth.actions';
 export class AppComponent implements OnInit {
   constructor(
     private readonly authStore$: Store<AuthAppState>,
-    private readonly router: Router
+    private readonly router: Router,
   ) {}
 
   ngOnInit() {
