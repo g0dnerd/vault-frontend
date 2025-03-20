28a9ea3b4ae0f5fd1a1fa8db7c7ef63c9ca53a8c
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-20 10:29:31 +0100

    Clean up routing hierarchy and provide additional state

--- src/app/tournaments/tournaments.routes.ts
+++ src/app/tournaments/tournaments.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { provideEffects } from '@ngrx/effects';
 import { provideState } from '@ngrx/store';
 
-import { Role } from '../_types';
 import { RolesGuard } from '../_helpers';
 import * as draftEffects from '../_store/effects/draft.effects';
 import * as enrollmentEffects from '../_store/effects/enrollment.effects';
 import * as imageEffects from '../_store/effects/image.effects';
 import * as matchEffects from '../_store/effects/match.effects';
+import * as phaseEffects from '../_store/effects/phase.effects';
 import * as playerEffects from '../_store/effects/player.effects';
 import * as standingsEffects from '../_store/effects/standings.effects';
 import * as tournamentEffects from '../_store/effects/tournament.effects';
@@ -15,21 +15,19 @@ import { draftReducer } from '../_store/reducers/draft.reducer';
 import { enrollmentReducer } from '../_store/reducers/enrollment.reducer';
 import { imageReducer } from '../_store/reducers/image.reducer';
 import { matchReducer } from '../_store/reducers/match.reducer';
+import { phaseReducer } from '../_store/reducers/phase.reducer';
 import { playerReducer } from '../_store/reducers/player.reducer';
 import { standingsReducer } from '../_store/reducers/standings.reducer';
 import { tournamentReducer } from '../_store/reducers/tournament.reducer';
+import { Role } from '../_types';
 
 import { AvailableTournamentsComponent } from './available-tournaments/available-tournaments.component';
 import { MyTournamentsComponent } from './my-tournaments/my-tournaments.component';
-import { TournamentDashboardComponent } from './dashboard/tournament-dashboard.component';
-import {
-  AdminTournamentDashboardComponent,
-  AdminDraftPanelComponent,
-  CreateTournamentComponent,
-  AdminTournamentListComponent,
-} from '../admin';
-import { LeagueDetailComponent } from './leagues/league-detail.component';
-import { CreateDraftComponent } from '../admin/create-draft.component';
+import { TournamentDashboardComponent } from './tournament-dashboard/tournament-dashboard.component';
+import { AdminDraftPanelComponent } from './admin/admin-draft-panel/admin-draft-panel.component';
+import { LeagueDetailComponent } from './leagues/league-detail/league-detail.component';
+import { CreateDraftComponent } from './admin/create-draft/create-draft.component';
+import { CreateTournamentComponent } from './admin/create-tournament/create-tournament.component';
 
 export const TOURNAMENT_ROUTES: Routes = [
   {
@@ -46,6 +44,8 @@ export const TOURNAMENT_ROUTES: Routes = [
       provideState('matches', matchReducer),
       provideEffects(imageEffects),
       provideState('images', imageReducer),
+      provideEffects(phaseEffects),
+      provideState('phases', phaseReducer),
       provideEffects(playerEffects),
       provideState('players', playerReducer),
       provideEffects(standingsEffects),
@@ -58,21 +58,26 @@ export const TOURNAMENT_ROUTES: Routes = [
   },
   {
     path: 'admin',
-    component: AdminTournamentListComponent,
-    data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
-    canActivate: [RolesGuard],
-  },
-  {
-    path: 'admin/create',
-    component: CreateTournamentComponent,
-    data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
-    canActivate: [RolesGuard],
-  },
-  {
-    path: 'admin/draft/create/:tournamentId',
-    component: CreateDraftComponent,
-    data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
-    canActivate: [RolesGuard],
+    children: [
+      {
+        path: 'create',
+        component: CreateTournamentComponent,
+        data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
+        canActivate: [RolesGuard],
+      },
+      {
+        path: ':tournamentId/draft/create',
+        component: CreateDraftComponent,
+        data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
+        canActivate: [RolesGuard],
+      },
+      {
+        path: ':tournamentId/draft/:draftId',
+        component: AdminDraftPanelComponent,
+        data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
+        canActivate: [RolesGuard],
+      },
+    ],
   },
   {
     path: ':tournamentId',
@@ -81,17 +86,5 @@ export const TOURNAMENT_ROUTES: Routes = [
   {
     path: 'league/:leagueId',
     component: LeagueDetailComponent,
-  },
-  {
-    path: 'admin/:tournamentId',
-    component: AdminTournamentDashboardComponent,
-    data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
-    canActivate: [RolesGuard],
-  },
-  {
-    path: 'admin/:tournamentId/draft/:draftId',
-    component: AdminDraftPanelComponent,
-    data: { requiredRoles: [Role.Admin, Role.PlayerAdmin] },
-    canActivate: [RolesGuard],
   },
 ];
