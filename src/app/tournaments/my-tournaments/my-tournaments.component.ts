69355dc084e5de5854de728a93d9be04df965d1d
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-03-22 10:41:53 +0100

    ECS-32 - Update styling of `AvailableTournamentsComponent` and merge obsolete `EnrollPanel` into it

--- src/app/tournaments/my-tournaments/my-tournaments.component.ts
+++ src/app/tournaments/my-tournaments/my-tournaments.component.ts
@@ -1,34 +1,28 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatListModule } from '@angular/material/list';
 import { RouterLink } from '@angular/router';
 import { PushPipe } from '@ngrx/component';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { Tournament } from '../../_types';
-import { selectEnrolledTournaments, State } from '../../_store';
-import {
-  initializePublicTournaments,
-  initializeEnrolledTournaments,
-} from '../../_store/actions/tournament.actions';
+import { selectAllTournaments, State } from '../../_store';
+import { initializeEnrolledTournaments } from '../../_store/actions/tournament.actions';
 
 @Component({
   standalone: true,
-  imports: [MatCardModule, MatListModule, NgFor, NgIf, RouterLink, PushPipe],
+  imports: [MatCardModule, NgFor, NgIf, RouterLink, PushPipe],
   templateUrl: './my-tournaments.component.html',
   styleUrl: './my-tournaments.component.scss',
 })
 export class MyTournamentsComponent implements OnInit {
   private readonly store$ = inject(Store<State>);
 
-  readonly enrolledTournaments$: Observable<Tournament[]> = this.store$.select(
-    selectEnrolledTournaments,
-  );
+  readonly enrolledTournaments$: Observable<Tournament[]> =
+    this.store$.select(selectAllTournaments);
 
   ngOnInit() {
-    this.store$.dispatch(initializePublicTournaments());
     this.store$.dispatch(initializeEnrolledTournaments());
   }
 }
