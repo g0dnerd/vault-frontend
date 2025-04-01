46e27b8d51d6747410614be8a4a0b391169e0eda
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-04-01 13:44:43 +0200

    Add prerender configuration

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'account/login',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'account/register',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'cubes',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
