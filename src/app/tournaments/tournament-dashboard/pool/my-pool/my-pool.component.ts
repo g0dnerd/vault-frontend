6f857acb353559dcfa69f6544bdac1b07dcd9c4c
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-04-04 11:18:33 +0200

    Add image layout and delete button Stub out a deck image layout and a button to delete individual images

--- src/app/tournaments/tournament-dashboard/pool/my-pool/my-pool.component.ts
+++ src/app/tournaments/tournament-dashboard/pool/my-pool/my-pool.component.ts
@@ -1,6 +1,8 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgIf } from '@angular/common';
 import { Component, inject, input, OnInit } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
 import { PushPipe } from '@ngrx/component';
 import { Store } from '@ngrx/store';
 import { distinctUntilChanged, map, Observable } from 'rxjs';
@@ -12,11 +14,20 @@ import {
 } from '../../../../_store';
 import { Image, PoolStatus } from '../../../../_types';
 import { ManagePoolComponent } from '../manage-pool/manage-pool.component';
+import { ImagesService } from '../../../../_services';
+import { deleteImage } from '../../../../_store/actions/images.actions';
 
 @Component({
   selector: 'app-my-pool',
   standalone: true,
-  imports: [ManagePoolComponent, MatExpansionModule, NgFor, NgIf, PushPipe],
+  imports: [
+    ManagePoolComponent,
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    NgIf,
+    PushPipe,
+  ],
   templateUrl: './my-pool.component.html',
   styleUrl: './my-pool.component.scss',
 })
@@ -29,11 +40,24 @@ export class MyPoolComponent implements OnInit {
     selectCurrentPoolStatus,
   );
 
+  constructor(private readonly imagesService: ImagesService) {}
+
   ngOnInit() {
     this.images$
       .pipe(
         distinctUntilChanged(),
         map((images) => console.log(JSON.stringify(images))),
+      )
+      .subscribe();
+  }
+
+  deleteImage(id: number) {
+    this.imagesService
+      .deleteImage(id)
+      .pipe(
+        map(({ id }) => {
+          this.store$.dispatch(deleteImage({ id }));
+        }),
       )
       .subscribe();
   }
